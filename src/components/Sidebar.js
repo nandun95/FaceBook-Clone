c7a3943d47298f914a5ae5143bcf4cfa28a9cce3
nandun95 <chamodyakumarasinghe@gmail.com>
2020-10-17 13:17:15 +0530

    Complete the Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './Sidebar.css';
 import SidebarRow from './SidebarRow'
-
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
+import PeopleIcon from '@material-ui/icons/People';
+import ChatIcon from '@material-ui/icons/Chat';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import profile from './img/nandun1.jpg'
 
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <SidebarRow title='pages'/>
-            <SidebarRow/>
-            <SidebarRow/>
-            <SidebarRow/>
-            <SidebarRow/>
+            <SidebarRow src={profile} title='Nandun S Chamodya'/>
+            <SidebarRow Icon={LocalHospitalIcon} title='CVID 19 Information Center' />
+            <SidebarRow Icon={EmojiFlagsIcon} title='Pages' />
+             <SidebarRow Icon={PeopleIcon} title='Friend' />
+             <SidebarRow Icon={ChatIcon} title='Messenger' />
+             <SidebarRow Icon={StorefrontIcon} title='Marketplace' />
+             <SidebarRow Icon={VideoLibraryIcon} title='Videos' />
+             <SidebarRow Icon={ExpandMoreIcon} title='Marketplace' />
         </div>
     )
 }
